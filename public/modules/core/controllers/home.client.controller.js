ea996753d50247eeb34424fdd13c3d2ce6ce03d9
benmalcom <benjaminikeji@gmail.com>
2015-05-09 16:43:23 +0100

    sindodo

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,10 +5,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     function ($scope, Authentication, $http, $location) {
         // This provides Authentication context.
         $scope.authentication = Authentication;
+        $scope.visible = true;
+        $scope.searching = false;
 
         // Create new Buzzrequest
         $scope.search = function () {
-
+            $scope.searching = true;
             $http({
                 url: '/search',
                 method: 'GET',
@@ -17,6 +19,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                 }
             }).success(function (response) {
                 $scope.postings = response.postings;
+                $scope.visible = false;
+                $scope.searching = false;
             }).error(function (error) {
                 alert('error');
             });
